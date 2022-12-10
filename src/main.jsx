20e1480a01e3cc3f3a30a469594ef9a6ad16af7e
giselars <gisela-rs@outlook.com>
2022-12-10 16:23:50 +0100

    show pokemons on app component

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
+import { PokemonApp } from './PokemonApp'
 
 import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      {/* <App /> */}
+      <PokemonApp/>
     </Provider>
   </React.StrictMode>
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "./slices/counter";
+import { pokemonSlice } from "./slices/pokemon";
 
 export const store = configureStore({
     reducer: {
-        counter: counterSlice.reducer
+        counter: counterSlice.reducer,
+        pokemons: pokemonSlice.reducer
     },
 })
 
